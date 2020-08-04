21bc461d5e1c435284180788c8898baccc533c0a
MoTong <17788730976@189.cn>
2020-08-04 21:01:47 +0800

    关于websocket技术探讨

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # nodejs-WS
-关于websocket技术探讨
+
+> 关于websocket技术探讨
+
+## Installation
+
+`cnpm i`
+
+## Startup project
+
+`npm run serve`
+
+## license
+
+Copyright (c) 2020 FEMotong
---
+++ app.js
@@ -0,0 +1,17 @@
+let app = require('express')();
+let WebSocket = require('ws');
+
+let wss = new WebSocket.Server({ port: 8080 });
+const port = 3000
+
+wss.on('connection', function connection(ws) {
+    console.log(`已经连接客户端!`);
+
+    ws.on('message', function incoming(message) {
+        console.log(`来自客户端信息： ${message}`);
+    });
+
+    ws.send(`WS发来的贺电！`);
+});
+
+app.listen(port ,() => console.log(`App listening on port ${port}......`));
